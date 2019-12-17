1f11ba31d987e70e5ad6936a17f7b6aeb5e647ce
sh4y <a.iyer@devnet.de>
2019-12-17 12:40:11 +0100

    Fixed functionality to detect accuracy

--- Net.py
+++ Net.py
@@ -6,15 +6,18 @@ import Data
 
 SOLVERS = ['lbfgs', 'sgd', 'adam']
 ACTIVATION = ['identity', 'logistic', 'tanh', 'relu']
-MAX_HIDDEN_LAYERS = 3
-MAX_NEURONS_PER_LAYER = 4
+MIN_HIDDEN_LAYERS = 2
+MAX_HIDDEN_LAYERS = 5
+
+MIN_NEURONS_PER_LAYER = 2
+MAX_NEURONS_PER_LAYER = 6
 
 INITIAL_NUMBER_NEURAL_NETS = 1
 
 def create_hidden_layer_properties():
-    layers = list(np.ones(random.randint(1, MAX_HIDDEN_LAYERS)))
+    layers = list(np.ones(random.randint(MIN_HIDDEN_LAYERS, MAX_HIDDEN_LAYERS)))
     for i in range(len(layers)):
-        layers[i] = random.randint(1, MAX_NEURONS_PER_LAYER)
+        layers[i] = random.randint(MIN_NEURONS_PER_LAYER, MAX_NEURONS_PER_LAYER)
     return tuple(layers)
 
 def create_random_nn():
@@ -34,6 +37,13 @@ def initialize_nn_population(n=INITIAL_NUMBER_NEURAL_NETS):
 
     return classifiers
 
+def accuracy(pred, actual):
+    num_correct = 0
+    for x in range(len(pred)):
+        if pred[x].all() == actual[x].all():
+            num_correct+=1
+    return num_correct / float(len(pred))
+
 def train_classifiers(classifiers, xtrain, ytrain):
     for x in range(len(classifiers)):
         print("Training classifier number " + str(x))
@@ -43,13 +53,15 @@ def test_classifiers(classifiers, xtest, ytest):
     for x in range(len(classifiers)):
         print("Testing classifier number " + str(x))
         prediction = classifiers[x].predict(xtest)
-        test_accuracy = np.sum(prediction == ytest.flatten()) / float(prediction.shape[0])
+        print(prediction)
+        print(ytest)
+        test_accuracy = accuracy(prediction, ytest)
         print(test_accuracy)
 
 
 xtrain,ytrain,xtest,ytest = Data.load_data(50, 50)
-print("Loaded xtrain, ytrain with {0} points of data".format(xtrain.shape))
-
+print("Loaded xtrain, ytrain with {0} {1} points of data".format(xtrain.shape, ytrain.shape))
+print("Loaded xtest, ytest with {0} {1} points of data".format(xtest.shape, ytest.shape))
 classifiers = initialize_nn_population()
 train_classifiers(classifiers, xtrain, ytrain)
 
